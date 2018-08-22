3d01f0423eccbd6dfeef4c632c2166303422b940
Xin Li <nix.li@mail.utoronto.ca>
2018-08-22 14:08:58 -0700

    image train test data load complete

--- train_subimage.py
+++ train_subimage.py
@@ -1,12 +1,83 @@
 import tensorflow as tf
 import numpy as np
 import os
-from tensorflow.examples.tutorials.mnist import input_data
-
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+import gzip
 import tensorflow as tf
+from tensorflow.python.platform import gfile
+
+DATA = 0
+LABEL = 1
+
+# parse error for reading data from file
+class DataSetInvalidError(Exception):
+    pass
+
+# utility functions for reading and preparing datasets
+# ideas taken from tensorflow mnist tutorials
+def _read32(bytestream):
+  dt = np.dtype(np.uint32).newbyteorder('>')
+  return np.frombuffer(bytestream.read(4), dtype=dt)[0]
 
 
+def get_images(data):
+
+    with gzip.GzipFile(fileobj=data) as bytes:
+        magic = _read32(bytes)
+        if magic != 2051:
+            raise DataSetInvalidError("Wrong magic number")
+
+        items = _read32(bytes)
+        rows =  _read32(bytes)
+        cols =  _read32(bytes)
+
+        data_bytes =  bytes.read(rows*cols*items)
+        image_data = np.frombuffer(data_bytes, dtype=np.uint8)
+        image_data = image_data.reshape(items, rows, cols, 1)
+
+        return image_data
+
+
+def get_labels(data, num_classes=62):
+
+    with gzip.GzipFile(fileobj=data) as bytes:
+        magic = _read32(bytes)
+        if magic != 2049:
+            raise DataSetInvalidError("Wrong magic number")
+
+        items = _read32(bytes)
+        data_bytes = bytes.read(items)
+
+        labels = np.frombuffer(data_bytes, dtype=np.uint8)
+        with tf.Session() as s:
+            one_hot_labels = s.run(tf.one_hot(labels, num_classes))
+        return one_hot_labels
+
+
+def read_data(train, test):
+
+    with gfile.Open(train[DATA], "rb") as data:
+        train_data = get_images(data)
+
+    with gfile.Open(train[LABEL], "rb") as label:
+        train_label = get_labels(label)
+
+    with gfile.Open(test[DATA], "rb") as data:
+        test_data = get_images(data)
+
+    with gfile.Open(test[LABEL], "rb") as label:
+        test_label = get_labels(label)
+
+
+    return (train_data, train_label), (test_data, test_label)
+
+
+#returns a generator that produces data of size batch_size
+def data_generator(data, batch_size=500):
+
+    pass
+
+
+# cnn layers to construct the network
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
@@ -26,8 +97,23 @@ def max_pool_2x2(x):
                           strides=[1, 2, 2, 1], padding='SAME')
 
 
-def run(x_from_the_other_side):
+def train(x_from_the_other_side):
         os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+        # sicne our images are made to the same format as mnist, use mnist functions to import data
+
+        train_data_path = [os.path.join(os.getcwd(), "AlphaNumericData", f) for f in
+                 ("emnist-balanced-train-images-idx3-ubyte.gz",
+                  "emnist-balanced-train-labels-idx1-ubyte.gz")]
+
+        test_data_path = [os.path.join(os.getcwd(), "AlphaNumericData", f) for f in
+                 ("emnist-balanced-test-images-idx3-ubyte.gz",
+                  "emnist-balanced-test-labels-idx1-ubyte.gz")]
+
+        train, test = read_data(train_data_path, test_data_path)
+
+        train_data = data_generator(train[DATA])
+        test_data =  data_generator(test[DATA])
+
         x = tf.placeholder(tf.float32, [None, 784])
 
         W_conv1 = weight_variable([5, 5, 1, 32])
@@ -73,8 +159,9 @@ def run(x_from_the_other_side):
         sess = tf.Session()
         sess.run(init)
 
-        for i in range(500):
-            batch_xs, batch_ys = mnist.train.next_batch(50)
+        for i in range(2000):
+
+            batch_xs, batch_ys = next(train_data)
             if i % 100 == 0:
                 train_accuracy = accuracy.eval(session=sess, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 1.0})
                 print("step %d, training accuracy %.3f" % (i, train_accuracy))
@@ -85,4 +172,4 @@ def run(x_from_the_other_side):
                                                  feed_dict={x: x_from_the_other_side, keep_prob: 0.5}))
 
 if __name__ == "__main__":
-    run(1)
+    train(1)
